91785f5556919596b523d261605ced7fcb056206
samuel <samulevy@gmail.com>
2018-06-29 22:39:37 -0300

    Generates the photo_uploader and attaches it to Booking and User models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
+  mount_uploader :after_photo, PhotoUploader
+
   belongs_to :user
   belongs_to :car
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,6 @@
 class Car < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+
   belongs_to :user
   has_many :bookings
 end
