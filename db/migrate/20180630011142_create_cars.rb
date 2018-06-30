c75c29ec2dc7f192e2b0eb13c7fc73792f9300a1
samuel <samulevy@gmail.com>
2018-06-29 22:15:11 -0300

    Create car model and migration

---
+++ db/migrate/20180630011142_create_cars.rb
@@ -0,0 +1,15 @@
+class CreateCars < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cars do |t|
+      t.references :user, foreign_key: true
+      t.string :license_plate
+      t.string :color
+      t.string :name
+      t.string :model
+      t.string :brand
+      t.string :car_photo
+
+      t.timestamps
+    end
+  end
+end
