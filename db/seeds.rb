8fb3c6c8bd8cf151c1a29ba34763336b4886fe84
samuel <samulevy@gmail.com>
2018-06-30 15:48:38 -0300

    Improves seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,55 @@
-client1 = User.create!(email: 'client1@mail.com', password: '123456')
-client2 = User.create!(email: 'client2@mail.com', password: '123456')
-washer1 = User.create!(email: 'washer1@mail.com', password: '123456', washer: 2)
-washer2 = User.create!(email: 'washer2@mail.com', password: '123456', washer: 1)
+require 'faker'
 
-car1 = Car.create!(
-  user: client1,
-  name: 'fit',
-  license_plate: 'HVB2345',
-  brand: 'Fiat',
-  color: 'Silver',
-  # photo: 'teste'
-)
+puts "### Creating Users Candidates and Washers ###"
 
-car2 = Car.create!(
-  user: client2,
-  name: 'fiesta',
-  license_plate: 'CPB2047',
-  brand: 'Ford',
-  color: 'red',
-  # photo: 'teste'
-)
+20.times do |i|
+  puts "## User #{i} ##"
+  User.create!(email: Faker::Internet.free_email, password: '123456')
+end
 
-booking = Booking.create!(car: car1)
+5.times do |i|
+  puts "## Candidate #{i} ##"
+  User.create!(email: Faker::Internet.free_email, password: '123456', washer: 1)
+end
+
+10.times do |i|
+  puts "## Washer #{i} ##"
+  User.create!(email: Faker::Internet.free_email, password: '123456', washer: 2)
+end
+
+clients = User.where(washer: 0)
+pendig  = User.where(washer: 1)
+washers = User.where(washer: 2)
+
+puts "### Creating Cars ###"
+
+clients.each_with_index do |client, index|
+  puts "## Client #{index} ##"
+
+  rand(1..4).times do |i|
+    puts "## Car #{i} ##"
+
+    vin = Faker::Vehicle.vin
+    plate = "#{vin.first(3)}#{rand(0..9)}#{rand(0..9)}#{index}"
+
+    Car.create!(
+      user: client,
+      brand: Faker::Vehicle.manufacture,
+      model: Faker::Pokemon.name,
+      name: Faker::Artist.name,
+      license_plate: plate,
+      color: Faker::Color.color_name,
+      # photo: 'teste'
+    )
+  end
+end
+
+cars = Car.all
+
+puts "### Creating Bookings ###"
+
+cars.each_with_index do |car, i|
+  puts "## Booking #{i} ##"
+
+  Booking.create!(car: car)
+end
