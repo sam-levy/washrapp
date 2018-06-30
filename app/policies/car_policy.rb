1ab136ddfe37f6a9386e272d70b6c0167713398a
samuel <samulevy@gmail.com>
2018-06-30 16:07:03 -0300

    Creates application_policy and car_policy

---
+++ app/policies/car_policy.rb
@@ -0,0 +1,29 @@
+class CarPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    owner?
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    owner?
+  end
+
+  def destroy?
+    owner?
+  end
+
+  private
+
+  def owner?
+    record.user == user
+  end
+end
