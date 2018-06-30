ef9aaea6f59da15e889b7d6847fd4732f9258eca
samuel <samulevy@gmail.com>
2018-06-29 22:22:13 -0300

    Create Bookings model and migration

---
+++ db/migrate/20180630011952_create_bookings.rb
@@ -0,0 +1,16 @@
+class CreateBookings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bookings do |t|
+      t.references :user, foreign_key: true
+      t.references :car, foreign_key: true
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+      t.integer :status
+      t.datetime :date
+      t.string :after_photo
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_30_011142) do
+ActiveRecord::Schema.define(version: 2018_06_30_011952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "car_id"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "status"
+    t.datetime "date"
+    t.string "after_photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_bookings_on_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "cars", force: :cascade do |t|
     t.bigint "user_id"
@@ -45,5 +60,7 @@ ActiveRecord::Schema.define(version: 2018_06_30_011142) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cars"
+  add_foreign_key "bookings", "users"
   add_foreign_key "cars", "users"
 end
